8618c0c7984bf22a3cdc389068378cd38650ad23
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-05-22 14:50:07 +0530

    Remove unnecessary files

--- 3-input-output.py
+++
@@ -1,8 +0,0 @@
-name = input("Please enter your name:")
-age = input("Please enter your age:")
-print(f"Hey!!! My name is {name} and my age is {age}")
-
-her_age = int(input("Please enter your age:"))
-years_to_100 = 100 - her_age
-print(f"You will be 100 years old in {years_to_100} years")
-print(f"Hey!!! My name is {name} and my age is {her_age}")
--- 4-conditionals.py
+++
@@ -1,5 +0,0 @@
-temp = 40
-if temp > 40:
-  print("Extreme hot")
-else:
-  print("Not so extreme hot")
